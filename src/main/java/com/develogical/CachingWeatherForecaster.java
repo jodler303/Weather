052f94479069f0700985725196bb9172c61575dc
jodler303 <jodler303@freenet.de>
2018-07-04 15:43:45 +0100

    Done with 1 hour Cache

--- src/main/java/com/develogical/CachingWeatherForecaster.java
+++ src/main/java/com/develogical/CachingWeatherForecaster.java
@@ -9,6 +9,7 @@ import java.util.*;
 public class CachingWeatherForecaster implements MyForecaster {
     private MyForecaster delegate = null;
     private HashMap<String,Forecast> cache = new HashMap<String,Forecast>();
+    private HashMap<String,Date> tstamp = new HashMap<String,Date>();
 
     public CachingWeatherForecaster(MyForecaster delegate) {
         this.delegate = delegate;
@@ -19,11 +20,14 @@ public class CachingWeatherForecaster implements MyForecaster {
         if ( delegate == null ) return null;
 
         String key = region.toString() + day.toString();
-        if ( cache.containsKey(key) )
+        Date oneHourAgo = new Date(System.currentTimeMillis() - (60 * 60 * 1000));
+
+        if ( cache.containsKey(key) && tstamp.get(key).after(oneHourAgo) )
             return cache.get( key );
 
         Forecast f = delegate.forecastFor(region, day);
         cache.put( key, f );
+        tstamp.put( key, new Date() );
         return f;
     }
 }
